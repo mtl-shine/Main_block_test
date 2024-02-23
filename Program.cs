6fec24c3c01f9bc6ef039826676a49838c4f41ff
Tatiana Nikitina <e.mtl.shine@gmail.com>
2024-02-23 19:34:59 +0100

    Entering an array from console was added

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.Clear();
+string[] EnterArrayFromConsole()
+{
+    Console.Write("Please enter a size of array: ");
+    string[] array = new string[Convert.ToInt32(Console.ReadLine())];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write("Please, enter the element of array: ");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
