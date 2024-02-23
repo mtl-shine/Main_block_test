de144fcc610e0f8425c53959df004da8fc0f1c97
Tatiana Nikitina <e.mtl.shine@gmail.com>
2024-02-23 19:49:11 +0100

    The task was finalized. Result array contains only strings which length is less or even three

--- Program.cs
+++ Program.cs
@@ -30,3 +30,21 @@ int CalculateResultArraySize(string[] array, int limit)
     return j;
 }
 
+string[] GetResultArray(string[] array, int limit, int size)
+{
+    string[] resultArray = new string[size];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            resultArray[j] = array[i];
+            j++;
+        }
+    }
+    return resultArray;
+}
+
+string[] originArray = EnterArrayFromConsole();
+int arraySize = CalculateResultArraySize(originArray, 3);
+Console.Write($"[{String.Join(", ", GetResultArray(originArray, 3, arraySize))}]");
