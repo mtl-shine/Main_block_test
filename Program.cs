111f41bd350ccb8b0504d88d7d50f091b2b3a56d
Tatiana Nikitina <e.mtl.shine@gmail.com>
2024-02-23 20:37:33 +0100

    The point with 3 instead of limit inside GetResultArray method has been corrected. README has been written

--- Program.cs
+++ Program.cs
@@ -36,7 +36,7 @@ string[] GetResultArray(string[] array, int limit, int size)
     int j = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i].Length <= 3)
+        if (array[i].Length <= limit)
         {
             resultArray[j] = array[i];
             j++;
